ea638c4f42199ce1b2b9e9ad64c5d63dc6f8bc9e
Nilanchala <nilanchalapanigrahy@gmail.com>
2015-06-26 22:26:43 +0100

    Initial commit

---
+++ app/src/main/java/com/javatechig/gridviewexample/DetailsActivity.java
@@ -0,0 +1,36 @@
+package com.javatechig.gridviewexample;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
+public class DetailsActivity extends ActionBarActivity {
+    private TextView titleTextView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Setting details screen layout
+        setContentView(R.layout.activity_details_view);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        String title = getIntent().getStringExtra("title");
+        String image = getIntent().getStringExtra("image");
+
+        titleTextView = (TextView) findViewById(R.id.title);
+        imageView = (ImageView) findViewById(R.id.grid_item_image);
+
+        titleTextView.setText(Html.fromHtml(title));
+
+        //Downloading image from url
+        Picasso.with(this).load(image).into(imageView);
+    }
+}
